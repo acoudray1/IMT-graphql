234d41d25c39b265c2527810758812f45cce459d
acoudray1 <axel.hpm.coudray@gmail.com>
2020-11-16 10:25:15 +0100

    end tp3

--- server/index.js
+++ server/index.js
@@ -1,22 +1,57 @@
 const { gql, ApolloServer } = require('apollo-server')
+const { RESTDataSource } = require('apollo-datasource-rest')
+
+/**
+ * BeersAPI is used to call an API and get data
+ */
+class BeersAPI extends RESTDataSource {
+	constructor() {
+		super()
+		this.baseURL = 'https://api.punkapi.com/v2/'
+	}
+
+	async getBeer(id) {
+		const [result] = await this.get(`/beers/${id}`)
+		return result
+	}
+
+	async getBeers() {
+		const result = await this.get('/beers')
+		return result
+	}
+}
 
 const typeDefs = gql`
     type Query {
         hello(name: String): String
         n: Int
         flag: Boolean
+		beer(id: ID!): Beer
+		beers: [Beer!]
     }
+
+	type Beer {
+		id: ID!
+		name: String
+		description: String
+	}
 `
+
+const dataSources = () => ({
+	beersAPI: new BeersAPI()
+})
 
 const resolvers = {
 	Query: {
 		hello: (_, { name = 'World' }) => `Hello ${name}!`,
 		n: () => Math.round(Math.random()*100),
-		flag: () => true
-	}
+		flag: () => true,
+		beer: (_, { id }, { dataSources: { beersAPI } }) => beersAPI.getBeer(id),
+		beers: (_, __, { dataSources: { beersAPI } }) => beersAPI.getBeers()
+	},
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, dataSources })
 
 server.listen().then(({ url }) => {
 	console.info(`🚀  Server ready at ${url}`)
